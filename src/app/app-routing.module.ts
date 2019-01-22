950ad294260cd6128600f4441f88970a3f573fab
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-01-22 09:04:24 +0100

    Add export routingComponents and refactor app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,3 +19,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [EventsComponent, SpecialEventsComponent, LoginComponent, RegisterComponent ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { EventsComponent } from './components/events/events.component';
-import { SpecialEventsComponent } from './components/special-events/special-events.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    LoginComponent,
-    EventsComponent,
-    SpecialEventsComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,

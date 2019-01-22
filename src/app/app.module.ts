d62d7001957bca3d2e5673987ce743c16ad7ebd0
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-01-22 10:01:25 +0100

    Add HttpClientModule to make http call to the backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
@@ -15,6 +16,7 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

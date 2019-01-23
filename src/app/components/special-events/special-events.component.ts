d4361f9619179945c06cb6d112da01caeef921fe
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-01-23 12:04:31 +0100

    Manage 401 (unauthorized) status in SpecialEventsComp

--- src/app/components/special-events/special-events.component.ts
+++ src/app/components/special-events/special-events.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './../../services/event.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-special-events',
@@ -11,7 +13,8 @@ export class SpecialEventsComponent implements OnInit {
   public specialEvents = [];
 
   constructor(
-    private _eventService: EventService
+    private _eventService: EventService,
+    private _router: Router
   ) { }
 
   ngOnInit() {
@@ -22,7 +25,14 @@ export class SpecialEventsComponent implements OnInit {
               console.log("The response is  " + JSON.stringify(res));
               this.specialEvents = res;
             },
-            (err) => console.log("There is an error" + JSON.stringify(err))
+            (err) => {
+              console.log("There is an error" + JSON.stringify(err));
+              if (err instanceof HttpErrorResponse) {
+                if (err.status === 401) {
+                  this._router.navigate(['/login']);
+                }
+              }
+            }
           );
   }
 
